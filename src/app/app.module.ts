80e3054c6aa5710b839db5161179d8aecb4f6037
Your Name <Your Email>
2024-01-22 23:33:10 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeSkillsComponent } from './components/employee-skills/employee-skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeSkillsComponent
   ],
   imports: [
     BrowserModule
